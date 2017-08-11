9f1f19246b405440d595e4863ff3df299cdfb710
Стоян Димитров <stoyan@easyads.bg>
2017-08-11 16:26:35 +0300

    Initial commit

---
+++ background.js
@@ -0,0 +1,49 @@
+'use strict'
+
+function isTracking(param)
+{
+  const settings = '^(utm_|yclid|fb_|action_)'
+    , re = new RegExp(settings)
+
+  return re.test(param)
+}
+
+function cleanUrl(url)
+{
+  const u = new URL(url)
+    , params = u.search
+        .replace('?', '')
+        .split('&')
+        .reduce((params, param) => {
+          if (param && ! isTracking(param)) {
+            params.push(param)
+          }
+
+          return params
+        }, [])
+
+  let search = ''
+  if (params.length) {
+    search = '?' + params.join('&')
+  }
+
+  u.search = search
+
+  return u.href
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  (details) => {
+    const cleaned = cleanUrl(details.url)
+
+    if (cleaned === details.url) {
+      return
+    }
+
+    return {
+      redirectUrl: cleaned
+    }
+  },
+  {urls: ['<all_urls>']},
+  ['blocking']
+)
